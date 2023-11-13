d640f3d325f3ce77f1d88977801301b29ef63799
ashrafalmayah <ashrafalmayah00@gmail.com>
2023-11-13 18:57:29 +0300

    finish up design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: { 
+      colors: {
+        current: 'currentColor',
+        'gray-blue': '#62738d',
+        'dark-blue': '#203b5c',
+      },
+      fontFamily: {
+        'primary': ['Barlow Semi Condensed', 'sans-serif'],
+      },
+      backgroundImage: {
+        'radial-gradient': 'radial-gradient(circle at center, rgba(255, 255, 255, 0.15) 0%, rgba(255, 255, 255, 0.15) 40%, rgba(255, 255, 255, 0.10) 40%, rgba(255, 255, 255, 0.10) 55%, rgba(255, 255, 255, 0.05) 55%)',
+      },
+    },
   },
   plugins: [],
 }
